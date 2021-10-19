aa46bdd6119243fa7eae909028c2520c49d8b14e
Razvan Dumitriu <razvan@dumitriu.co.uk>
2021-10-19 23:59:48 +0300

    Added first .asm file

---
+++ p1.asm
@@ -0,0 +1,34 @@
+.data
+	x: .long 30
+	y: .long 7
+	sum: .space 4
+	dif: .space 4
+	prod: .space 4
+	cat: .space 4
+	rest: .space 4
+.text
+.globl _start
+
+_start:
+	mov x, %eax
+	add y, %eax
+	mov %eax, sum
+
+	mov x, %eax
+	sub y, %eax
+	mov %eax, dif
+
+	mov x, %eax
+	mov y, %ebx
+	imul %ebx
+	mov %eax, prod
+
+	mov x, %eax
+	mov y, %ebx
+	idiv %ebx
+	mov %eax, cat
+	mov %ebx, rest
+
+	mov $1, %eax
+	mov $0, %ebx
+	int $0x80
