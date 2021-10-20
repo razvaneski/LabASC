3167f11cd58bb8494820a31069d0300d4433843b
Razvan Dumitriu <razvan@dumitriu.co.uk>
2021-10-20 14:13:37 +0300

    Modified p1 files

--- p1.asm
+++ p1.asm
@@ -1,8 +1,8 @@
 .data
-    a: .long 5
-    b: .long -3
-    text1: .asciz "a >= b\n"
-    text2: .asciz "a < b\n"
+    a: .long -3
+    b: .long 5
+    text2: .asciz "a >= b\n"
+    text1: .asciz "a < b\n"
 .text
 .globl _start
 _start:
@@ -14,9 +14,8 @@ _start:
     mov $1, %ebx
     mov $text2, %ecx
     mov $7, %edx
-	
-	int $0x80
-	jmp exit
+    int $0x80
+    jmp exit
 et:
     mov $4, %eax
     mov $1, %ebx
