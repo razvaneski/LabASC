369dbe193b1c6c7b08393008ea330e00da1e42bd
Razvan Dumitriu <razvan@dumitriu.co.uk>
2021-11-12 22:06:38 +0200

    -

--- Tema Laborator/test1.asm
+++ Tema Laborator/test1.asm
@@ -10,10 +10,10 @@
 	y: .space 4
 	rezultat: .space 4
 
-	a: .asciz "add"
-	s: .asciz "sub"
-	m: .asciz "mul"
-	d: .asciz "div"
+	a: .ascii "add"
+	s: .ascii "sub"
+	m: .ascii "mul"
+	d: .ascii "div"
 
 .text
 
