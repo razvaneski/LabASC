c24babe168fcb3d48c824457a8b80fbcd9789fc2
Razvan Dumitriu <razvan@dumitriu.co.uk>
2021-11-12 18:34:05 +0200

    -

--- Tema Laborator/test1.asm
+++ Tema Laborator/test1.asm
@@ -1,9 +1,8 @@
 .data
-	str: .asciz "2 10 mul 5 div 7 6 sub add"
 	chDelim: .asciz " "
+	str: .asciz "2 10 mul 5 div 7 6 sub add"
+	res: .space 4
 	formatPrintf: .asciz "%s\n"
-	res: .space 4 
-	res2: .space 4
 .text
 
 .global main
@@ -11,53 +10,20 @@
 main:
 	pushl $chDelim
 	pushl $str
-	call strtok 
-	popl %ebx
-	popl %ebx
-	
-	movl %eax, res
-	
-	pushl res				# este deja un pointer
-	pushl $formatPrintf
-	call printf
-	popl %ebx
-	popl %ebx
-	
-et_for:
-	pushl $chDelim
-	pushl $0
 	call strtok
 	popl %ebx
-	popl %ebx 
-	
-	cmp $0, %eax
-	je exit
-	
-	movl %eax, res
-
-	pushl %eax
-	pushl %ecx
-	pushl %edx
-
-	pushl res
-	call atoi
 	popl %ebx
 
-	movl %eax, res2
-
-	popl %edx
-	popl %ecx
-	popl %eax
-	
-	pushl res2				# este deja un pointer
+	movl %eax, res
+	pushl res
 	pushl $formatPrintf
 	call printf
 	popl %ebx
 	popl %ebx
-	
-	jmp et_for	
 
 exit:
-	movl $1, %eax
+	mov $1, %eax
 	xorl %ebx, %ebx
 	int $0x80
+
+
