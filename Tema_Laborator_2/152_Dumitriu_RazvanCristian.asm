e3b6a66585231e0fa5c172e9db56c64629e5f2ae
Razvan Dumitriu <razvan@dumitriu.co.uk>
2021-12-29 17:13:08 +0200

    -

--- Tema_Laborator_2/152_Dumitriu_RazvanCristian.asm
+++ Tema_Laborator_2/152_Dumitriu_RazvanCristian.asm
@@ -138,6 +138,11 @@ bkt:
 	cmp $0, %eax
 	jl bkt_next
 
+	movl %eax, i
+	incl i # i = perm[curr] + 1
+	decl (%esi, %eax, 4) # f[perm[curr]] -= 1
+	movl $0, (%edi, %ecx, 4) # perm[curr] = 0
+
 	incl curr
 	jmp bkt
 
